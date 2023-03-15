9a3826b541515f1a1ee84fe265a008c87bc5b539
Charline Dutel le vaillant <cdutel-l@z2r7p6.42lyon.fr>
2023-03-15 12:14:51 +0100

    add cpp01 ex02

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cdutel-l <cdutel-l@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 12:03:33 by cdutel-l          #+#    #+#             */
+/*   Updated: 2023/03/15 12:14:00 by cdutel-l         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <string>
+
+int	main()
+{
+	std::string		str = "HI THIS IS BRAIN";
+	std::string*	stringPTR = &str;
+	std::string&	stringREF = str;
+
+	std::cout << &str << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << str << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
+	return (0);
+}
