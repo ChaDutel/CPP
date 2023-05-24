1b256fa31df849a66382133b5344ac3420212415
ChaDutel <cdutel-l@z2r4p5.42lyon.fr>
2023-05-24 15:10:48 +0200

    add ex02cpp08

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,77 @@
+#ifndef	MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <vector>
+# include <exception>
+# include <algorithm>
+# include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack() : std::stack<T>() {}
+		MutantStack(const MutantStack<T>& other) : std::stack<T>(other) {}
+		MutantStack<T>&	operator=(const MutantStack<T>& other)
+		{
+			this->c = other.c;
+			return (*this);
+		}
+		~MutantStack() {}
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator constIterator;
+
+		iterator	begin()
+		{
+			return (this->c.begin());
+		}
+
+		iterator	end()
+		{
+			return (this->c.end());
+		}
+
+		constIterator	begin() const
+		{
+			return (this->c.begin());
+		}
+
+		constIterator	end() const
+		{
+			return (this->c.end());
+		}
+};
+
+
+// template<typename T>
+// class MutantStack : public std::stack<T>
+// {
+// 	public:
+// 		typedef typename std::stack<T>::container_type::iterator iterator;
+
+// 		MutantStack() : std::stack<T>() {}
+// 		MutantStack(const MutantStack<T>& other) : std::stack<T>(other) {}
+// 		virtual ~MutantStack() {}
+
+// 		MutantStack<T>& operator=(const MutantStack<T>& other)
+// 		{
+// 			if (this != &other) {
+// 				this->c = other.c;
+// 			}
+// 			return *this;
+// 		}
+
+// 		iterator begin()
+// 		{
+// 			return this->c.begin();
+// 		}
+
+// 		iterator end()
+// 		{
+// 			return this->c.end();
+// 		}
+// };
+
+# endif
